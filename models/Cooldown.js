44d55c9fc4ad9151c6293dd3a6825fe933a950e6
𝑻𝒉𝒐𝒎𝒂𝒔 <erdemerdem11221154@gmail.com>
2026-07-06 18:17:44 +0300

    Delete models directory

--- models/Cooldown.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const cooldownSchema = new Schema({
-    userId: { type: String, required: true, unique: true },
-    command: { type: String, required: true },
-    expiresAt: { type: Date, required: true }
-    // Cooldown time is a variable. We will set it when we create a cooldown entry.
-});
