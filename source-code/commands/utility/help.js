39a78931d9e572cddc19133980cf78dbcf94ea58
𝑻𝒂𝒏𝒐𝒔 <erdemerdem11221154@gmail.com>
2026-07-21 22:39:56 +0300

    Update help command for Luna OS

--- source-code/commands/utility/help.js
+++ source-code/commands/utility/help.js
@@ -4,22 +4,22 @@ const User = require('../../models/User');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('help')
-        .setDescription('View all available Athena systems.'),
+        .setDescription('View all available Luna systems.'),
     async execute(interaction) {
         let userData = await User.findOne({ userId: interaction.user.id });
         if (!userData) userData = await User.create({ userId: interaction.user.id });
 
         const helpEmbed = new EmbedBuilder()
-            .setAuthor({ name: 'Athena OS', iconURL: interaction.client.user.displayAvatarURL() })
+            .setAuthor({ name: 'Luna OS', iconURL: interaction.client.user.displayAvatarURL() })
             .setTitle('Void System')
             .setDescription(`There's. commands and its categories so you could execute them when you wish!`)
             .addFields(
-                { name: '💳 Economy', value: '`/profile` `/daily` `/work` `/give` `/leaderboard` `/coinflip`', inline: false },
-                { name: '📈 Market', value: '`/setup-market`', inline: false },
-                { name: '⚙️ Tools', value: '`/help`', inline: false }
+                { name: `${emojis.work} Economy`, value: '`/profile` `/daily` `/work` `/give` `/leaderboard` `/coinflip`', inline: false },
+                { name: `${emojis.market} Market`, value: '`/setup-market`', inline: false },
+                { name: `${emojis.tools} Tools`, value: '`/help`', inline: false }
             )
             .setColor('#F5F5F7')
-            .setFooter({ text: 'Designed by Athena' });
+            .setFooter({ text: 'Designed by Luna' });
 
         const row = new ActionRowBuilder().addComponents(
             new ButtonBuilder()
